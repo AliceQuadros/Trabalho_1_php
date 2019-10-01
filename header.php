ea92781479151dd35157024728b66c9afca537ec
kgcoelho <karinegarciacoelho@gmail.com>
2019-10-01 15:53:34 -0300

    ajustes gerais

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <!-- header -->
 <header class="header">
 <div class="container">
-            <div>
-                <a href="home.php" class="grid-4 ">
-                    <img src="LOGO DO SITE" alt="alguma coisa">
+            <div class="logo">
+                <a href="?p=home" class="grid-4 ">
+                    <img src="img/logo_alika.png" alt="Logo ALIKA">
                 </a>
             </div>
             <nav class="grid-12 header-menu">
                 
-                    <a href="?p=home.php" class="active">Home</a>
+                    <a href="?p=home" class="active">Home</a>
                     <a href="?p=sobre">Sobre</a>
                     <a href="?p=form">Contate-nos</a>
                     <!--<img src="img/search.png" alt="pesquisar">-->
--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 
 <head>
     <meta charset="utf-8">
-    <title>TROCAR</title>
+    <title>ALIKA</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.ico" />
 
 
     
