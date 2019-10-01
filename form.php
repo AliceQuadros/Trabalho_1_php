a30bff14d6a9f1284749f843d70ad0c4374069cd
Alice Quadros <alicequadros63@gmail.com>
2019-10-01 11:50:08 -0300

    mudança do placeholder do telefone

--- form.php
+++ form.php
@@ -15,7 +15,7 @@
             <label for="email">E-mail</label>
             <input type="email" value='<?=@$email?>' placeholder="testando@lalala.com" id="email">
             <label for="telefone">Telefone</label>
-            <input type="number" value='<?=$fone?>' placeholder="(53) 69696969" id="telefone">
+            <input type="number" value='<?=$fone?>' placeholder="(53) 999999999" id="telefone">
             <label for="espec">Especificações</label>
             <textarea id="espec" value='<?=$mensagem?>' placeholder="Digite sua Mensagem..."></textarea>
             <button type="submit" class="btn">Enviar</button>
