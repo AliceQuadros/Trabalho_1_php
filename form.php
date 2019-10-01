2bc607623c8cc4447909a9c43dfa0a78d1f288a2
Alice Quadros <alicequadros63@gmail.com>
2019-10-01 16:10:16 -0300

    atualização do php

--- form.php
+++ form.php
@@ -1,22 +1,24 @@
 <section class="contato container">
-        <form action="" id="form-orcamento" class="contato-form grid-8">
+        <form action="" id="form-orcamento" class="contato-form grid-8" enctype="multipart/form-data">
             <div class="row">
                 <div class="grid-4">
                     <label for="nome">Nome</label>
-                    <input type="text" value='<?=@$nome?>' placeholder="Nome" id="nome">
+                    <input type="text" value='<?=@$nome?>' placeholder="Nome" name="nome">
                 </div>
                 <div class="grid-4">
 
                     <label for="sobrenome" value=''>Sobrenome</label>
-                    <input type="text" value='<?=@$sobrenome?>' placeholder="sobrenome" id="sobrenome">
+                    <input type="text" value='<?=@$sobrenome?>' placeholder="sobrenome" name="sobrenome">
                 </div>
             </div>
 
             <label for="email">E-mail</label>
-            <input type="email" value='<?=@$email?>' placeholder="testando@lalala.com" id="email">
+            <input type="email" value='<?=@$email?>' placeholder="testando@lalala.com" name="email">
             <label for="telefone">Telefone</label>
-            <input type="number" value='<?=$fone?>' placeholder="(53) 999999999" id="telefone">
+            <input type="number" value='<?=$fone?>' placeholder="(53) 999999999" name="telefone">
             <label for="espec">Especificações</label>
-            <textarea id="espec" value='<?=$mensagem?>' placeholder="Digite sua Mensagem..."></textarea>
+            <label for="imagem">Enviar Imagem</label>
+            <input type="file" name="upload">
+            <textarea name="mensagem" value='<?=$mensagem?>' placeholder="Digite sua Mensagem..."></textarea>
             <button type="submit" class="btn">Enviar</button>
         </form>
---
+++ incluir.php
@@ -0,0 +1,41 @@
+<?php
+@$titulo = $_REQUEST['titulo'];
+@$texto = $_REQUEST['texto'];
+@$imagem =  $_REQUEST['imagem'];
+@$posicao  = $_REQUEST['posicao'];
+
+if(isset($_REQUEST['botao'])){
+    
+    $erros = validaForm($_REQUEST, array('nome:texto:Nome é obrigatório',
+                                         'sobrenome:texto:Sobrenome é obrigatório',
+                                         'email:email:Deve ser um e-mail válido'));
+   if (strlen($erros)==0) {
+    
+    $id = 0;
+    $sql = "INSERT INTO `artigo` (`artTitul`,`artTexto`,`artImage`, `artImpos`) VALUES (?,?,?,?);";
+    $retorno = fazConsultaSegura($sql,array($titulo,
+                                            $texto,
+                                            $imagem,
+                                            $posicao,),$id);
+    
+    if (isset($_FILES['arquivoParaUpload'])) {
+        include("upload.php");
+       
+       }
+    
+    $titulo = '';     
+    $texto = '';
+    $imagem =  '';
+    $posicao =  '';
+   }
+   else {
+     echo ("$erros<hr>");
+      include ("incluir_form.php");
+   }
+
+    
+}
+else {
+    include ("incluir_form.php");
+}
+?>
